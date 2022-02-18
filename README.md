0de0965ce0d769fe66e322489ada52aae2a9d2ac
Aleksandra <dubiel.aleksandra.88@gmail.com>
2022-02-18 11:43:05 +0100

    Initial project add

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SGF-pet-clinic
+
 This is the pet-clinic project from Spring, part of Spring beginner to guru Udemy course
---
+++ src/test/java/ad/sgfpetclinic/SgfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ad.sgfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
